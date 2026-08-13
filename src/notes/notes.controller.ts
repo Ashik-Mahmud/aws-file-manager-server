0c1ffa90fcbdd1aff79cda1353c61ee5853ca448
Ashik Mahmud <ashikmamud187@gmail.com>
2026-08-13 07:13:02 +0600

    feat: enhance Notes API response with additional metadata and implement Dockerfile for containerization

--- src/notes/notes.controller.ts
+++ src/notes/notes.controller.ts
@@ -12,12 +12,18 @@ export class NotesController {
         description: 'Returns the status of the Notes API.',
     })
     @Get()
-    getNotes(): { message: string, status?: number, environment: string, port: string } {
+    getNotes(): { message: string, status?: number, environment: string, port: string, updatedAt: string, isAutomated: boolean, info: { description: string, version: string } } {
 
         return {
             message: 'Notes API is working',
             environment:  process.env.NODE_ENV ?? 'development',
             port: process.env.PORT ?? '3000',
+            updatedAt: new Date().toISOString(),
+            isAutomated: true,
+            info: {
+                description: 'This is a sample Notes API built with NestJS and DynamoDB.',
+                version: '1.0.0',
+            },
         };
     }
 }
